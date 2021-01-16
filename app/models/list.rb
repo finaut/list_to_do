2843eea36af3d7842f461d947069cc55bac12646
finaut477@gmail.com <finaut477@gmail.com>
2021-01-15 17:53:40 -0800

    add logic capitalize, validates for model task and list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,9 @@
 class List < ApplicationRecord
   has_many :tasks
+  validates :name, presence: true, length: {maximum: 255}
+  before_save :capitalize_name
+
+  def capitalize_name
+    self.name.capitalize!
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :list
+  validates :title, presence: true, length: { maximum: 255}
+  before_save :capitalize_title
+
+  def capitalize_title
+    self.title.capitalize!
+  end
 end
